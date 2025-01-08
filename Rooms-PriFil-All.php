006e52801fbb596a570a89fbe379e0693cd16b2e
snakeyb <60200653+snakeyb@users.noreply.github.com>
2025-01-08 23:36:33 +0000

    Create Rooms-PriFil-All.php

---
+++ Rooms-PriFil-All.php
@@ -0,0 +1,15 @@
+<?php
+namespace Espo\Custom\Classes\Select\Rooms\PrimaryFilters;
+
+use Espo\Core\Select\Primary\Filter;
+use Espo\ORM\Query\SelectBuilder;
+
+class All implements Filter
+{
+    public function apply(SelectBuilder $queryBuilder): void
+    {
+        $queryBuilder->where([
+          [true => true]
+        ]);
+    }
+}
